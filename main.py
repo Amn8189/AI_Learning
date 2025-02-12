48875b2527696d8efed0b31766a0ddf813a4ac87
Amn8189 <123394314+Amn8189@users.noreply.github.com>
2025-02-12 18:25:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
-app.wsgi_app.wsgi_app = ProxyFix(
-    app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )# allow app to access ip and https
 
 #http - https
